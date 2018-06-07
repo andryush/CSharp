aa5c21ea06b95bcb982683ed003a0d0da20b1b39
andrey <andryush333@gmail.com>
2018-06-07 10:51:09 +0400

    cube homerwork

---
+++ cubeHomework/cubeHomework/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace cubeHomework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 19;
+            string symbol = "";
+
+
+            for (int row = 0; row < n; row++)
+            {
+                for (int column = 0; column < n; column++)
+                {
+                    if (column > row)
+                    {
+                        symbol += "- ";
+
+                    }
+                    else 
+                    {
+                        symbol += "* ";
+                    }
+
+                }
+                symbol += "\n";
+
+            }
+            Console.Write(symbol);
+        }
+    }
+}
