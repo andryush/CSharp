6f4cf16fd4c4f0af3ad916ff6190d5979cdb1e4f
Andrey <andryush333@gmail.com>
2018-06-25 17:25:51 +0400

    classhomeworkfinal

---
+++ ClassHomeworkNew/ClassHomeworkNew/GeometricForm.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassHomeworkNew
+{
+    class GeometricForm
+    {
+        public double pi = Math.PI;
+
+        public int length;
+        public int width;
+        public int radius;
+        
+
+    }
+}
---
+++ ClassHomeworkNew/ClassHomeworkNew/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ClassHomeworkNew
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
