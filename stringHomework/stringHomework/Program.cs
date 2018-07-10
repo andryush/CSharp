cc1cc529ae8417a41c30410563405cc4cc172895
Andrey <andryush333@gmail.com>
2018-07-10 17:03:08 +0400

    Ceasar's Cipher

--- stringHomework/stringHomework/Program.cs
+++ stringHomework/stringHomework/Program.cs
@@ -6,14 +6,65 @@ namespace stringHomework
     {
         static void Main(string[] args)
         {
-            string MyText = "This too shall pass".ToLower();
-            char symbol = ' ';
+            string myText = "This too shall pass".ToLower();
+            string alphabet = "abcdefghijklmnopqrstuvwxyz";
+            string result = string.Empty;
 
-            for (int i = 0; i < MyText.Length; i++)
+            int key = 3;
+            int position;
+            int positionKey;
+            
+            char eachChar = ' ';
+
+
+            //Console.WriteLine(Encode(myText));
+            Console.WriteLine(Decode("wklvcwrrcvkdoocsdvvw"));
+
+
+            //Function for text Encoding
+            string Encode(string textToEncode)
             {
-                   
+                for (int i = 0; i < textToEncode.Length; i++)
+                {
+                    eachChar = textToEncode[i];
+                    position = alphabet.IndexOf(eachChar);
+                    if ((position + key) > alphabet.Length)
+                    {
+                        positionKey = position - 0;
+                    }
+                    else
+                    {
+                        positionKey = position + key;
+                    }
+                    
+                    result += alphabet[positionKey];
+                }
+                return result;
             }
-            Console.WriteLine(MyText.Length);
+
+            //Function for Decoding
+            string Decode(string textToDecode)
+            {
+                for (int i = 0; i < textToDecode.Length; i++)
+                {
+                    eachChar = textToDecode[i];
+                    position = alphabet.IndexOf(eachChar);
+                    if((position - key) < alphabet.IndexOf("a"))
+                    {
+                        positionKey = position + 0;
+                    }
+                    else
+                    {
+                        positionKey = position - key;
+                    }
+                    
+                    result += alphabet[positionKey];
+                }
+                return result;
+            }
+
+
+            Console.ReadLine();
         }
     }
 }
